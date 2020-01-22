f696f6eb3f6b531269767395c93a7816c9671264
Derek M Berger <dmberger.dev@gmail.com>
2020-01-22 11:22:11 -0500

    update test labels to identify manual (blocking) tests

--- empyricalRMT/tests/test_expected.py
+++ empyricalRMT/tests/test_expected.py
@@ -1,11 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import os
 import pytest
-import seaborn as sbn
-
-from pathlib import Path
-from statsmodels.nonparametric.kde import KDEUnivariate as KDE
 
 import empyricalRMT.rmt.expected as expected
 
@@ -14,14 +8,10 @@ from empyricalRMT.rmt.plot import spacings as plotSpacings
 from empyricalRMT.rmt.unfold import Unfolder
 
 
-@pytest.mark.expected
+@pytest.mark.plot
 def test_GOE():
-    KDE_POINTS = 1000
     for i in range(1):
         M = generateGOEMatrix(1000)
         eigs = np.sort(np.linalg.eigvals(M))
         unfolded = Unfolder(eigs).unfold(trim=False)
         plotSpacings(unfolded, bins=20, kde=True, mode="block")
-
-
-test_GOE()
