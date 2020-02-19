aaea90a5b0cd9e16f41fd4cb09b16cf625cdf08e
Derek M Berger <dmberger.dev@gmail.com>
2020-02-19 18:44:59 -0500

    update Compare to allow comparison to single curve

--- empyricalRMT/rmt/compare.py
+++ empyricalRMT/rmt/compare.py
@@ -9,7 +9,13 @@ from empyricalRMT._validate import make_1d_array
 
 
 class Compare:
-    def __init__(self, curves: List[ndarray], labels: List[str]):
+    def __init__(
+        self,
+        curves: List[ndarray],
+        labels: List[str],
+        base_curve: ndarray = None,
+        base_label: str = None,
+    ):
         """Construct a Compare object for accessing various comparison methods.
 
         Parameters
@@ -21,18 +27,28 @@ class Compare:
         labels: List[str]
             A list of strings identifying each curve. Must be the same length as curves,
             and labels[i] must be the label for curves[i], for all valid values of i.
+        base_curve: ndarray
+            The base curve against which each curve of `curves` will be compared, if the
+            desire is to compare multiple curves only to one single curve.
+        base_label: str
+            The label for identifying the base_curve.
         """
         self.curves = [make_1d_array(curve) for curve in curves]
         self.labels = labels.copy()
+        self.base_curve = make_1d_array(base_curve) if base_curve else None
+        self.base_label = base_label  # don't need to copy strings in Python
         self.__validate_curve_lengths()
         self.dict = dict(zip(self.labels, self.curves))
 
     def correlate(self) -> DataFrame:
         """Return the grid of correlations across curves. """
         self.__validate_curve_lengths(
-            message="Comparing via correlation requires curves all have identical lengths",
+            message="Comparing via correlation requires all curves have identical lengths",
             check_all_equal=True,
         )
+        if self.base_curve is not None:
+            data = np.corrcoef(self.base_curve, self.curves)[0]
+            return pd.DataFrame(data=data, index=self.labels, columns=[self.base_label])
         data = np.corrcoef(self.curves)
         return pd.DataFrame(data=data, index=self.labels, columns=self.labels)
 
@@ -48,5 +64,10 @@ class Compare:
             raise ValueError("`labels` must have the same length as `curves`.")
 
         all_equal = np.all([len(curve) == len(curves[0]) for curve in curves])
-        if check_all_equal and not all_equal:
-            raise ValueError(message)
+        if check_all_equal:
+            if self.base_curve is not None and self.base_label is not None:
+                if len(curves[0]) != len(self.base_curve):
+                    raise ValueError(message)
+            if not all_equal:
+                raise ValueError(message)
+
--- empyricalRMT/tests/test_correlate.py
+++ empyricalRMT/tests/test_correlate.py
@@ -10,18 +10,19 @@ from empyricalRMT.rmt.correlater import _compute_upper_correlations
 @pytest.mark.math
 @pytest.mark.fast
 def test_correlate() -> None:
-    n_curves = 2
-    curves = [np.random.standard_normal(100) for _ in range(n_curves)]
-    labels = [str(i) for i in range(n_curves)]
-    compare = Compare(curves, labels)
-    df = compare.correlate()
+    for n_curves in range(2, 7):
+        n_curves = 2
+        curves = [np.random.standard_normal(100) for _ in range(n_curves)]
+        labels = [str(i) for i in range(n_curves)]
+        compare = Compare(curves, labels)
+        df = compare.correlate()
 
-    # basic sanity / construction checks
-    assert np.all(df.index == labels)
-    assert np.all(df.columns == labels)
+        # basic sanity / construction checks
+        assert np.all(df.index == labels)
+        assert np.all(df.columns == labels)
 
     curves = [np.random.standard_normal(100) for _ in range(1)]
-    labels = [str(i) for i in range(n_curves)]
+    labels = [str(i) for i in range(len(curves))]
 
     # check that can't have less than 2 curves
     with pytest.raises(ValueError):
@@ -32,7 +33,9 @@ def test_correlate() -> None:
     with pytest.raises(ValueError):
         compare = Compare(curves, labels)
 
-    curves = [np.random.standard_normal(np.random.choice(100)) for _ in range(3)]
+    # check that curves must be consistent
+    sizes = [3, 4, 5]
+    curves = [np.random.standard_normal(size) for size in sizes]
     labels = [str(i) for i in range(len(curves))]
     with pytest.raises(ValueError):
         compare = Compare(curves, labels).correlate()
