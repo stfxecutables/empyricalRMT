c055051a1752f391b20a10df5d81b341f99dee54
Derek M Berger <dmberger.dev@gmail.com>
2020-02-19 13:06:00 -0500

    update test_construct to use Eigenvalues class

--- empyricalRMT/tests/test_construct.py
+++ empyricalRMT/tests/test_construct.py
@@ -1,16 +1,16 @@
 import pytest
 
 from empyricalRMT.rmt.construct import fast_poisson_eigs
+from empyricalRMT.rmt.eigenvalues import Eigenvalues
 from empyricalRMT.rmt.plot import _spacings as plotSpacings
-from empyricalRMT.rmt.unfolder import Unfolder
 
 
 @pytest.mark.construct
 @pytest.mark.fast
 def test_fast_poisson() -> None:
     for i in range(1):
-        eigs = fast_poisson_eigs(5000)
-        unfolded = Unfolder(eigs).unfold(trim=False)
+        vals = fast_poisson_eigs(5000)
+        unfolded = Eigenvalues(vals).unfold()
         plotSpacings(
             unfolded, title="Fast Poisson Spacing Test", bins=20, kde=True, mode="block"
         )
