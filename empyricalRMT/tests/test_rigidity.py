5df059627d3cec03d41339d54288cf0377f3d279
Derek M Berger <dmberger.dev@gmail.com>
2020-02-25 14:05:02 -0500

    implement testing of averaged spectral rigidity

--- empyricalRMT/tests/test_rigidity.py
+++ empyricalRMT/tests/test_rigidity.py
@@ -1,7 +1,10 @@
+import numpy as np
+import pandas as pd
 import pytest
 
 from empyricalRMT.rmt.construct import generate_eigs, goe_unfolded
 from empyricalRMT.rmt.eigenvalues import Eigenvalues
+from empyricalRMT.rmt.plot import _spectral_rigidity
 
 
 @pytest.mark.plot
@@ -18,7 +21,32 @@ def test_plot_rigidity() -> None:
     # eigs = Eigenvalues(generate_eigs(2000, log=True))
     # unfolded = eigs.unfold(smoother="goe", degree=9)
 
-    unfolded = goe_unfolded(5000)
+    unfolded = goe_unfolded(20000, log=True)
     unfolded.plot_nnsd(mode="noblock")
     unfolded.plot_level_variance(mode="noblock")
-    unfolded.plot_spectral_rigidity(max_L=100, c_iters=10000)
+    unfolded.plot_spectral_rigidity(max_L=200, c_iters=20000)
+
+
+@pytest.mark.plot
+def test_average_rigidity() -> None:
+    n = 5
+    size = 5000
+    L, delta3 = None, []
+    for i in range(n):
+        unfolded = goe_unfolded(size, log=True)
+        df = unfolded.spectral_rigidity(
+            min_L=5, max_L=100, c_iters=10000, show_progress=True
+        )
+        if i == 0:
+            L = df["L"]
+        delta3.append(df["delta"].to_numpy())
+
+    delta3 = np.mean(np.array(delta3), axis=0)  # vertically stacked rows of delta3
+    df = pd.DataFrame({"L": L, "delta": delta3})
+    _spectral_rigidity(
+        unfolded=None,
+        data=df,
+        title=f"Rigidity: average of {n} N={size} GOE",
+        ensembles=["goe", "poisson"],
+    )
+
