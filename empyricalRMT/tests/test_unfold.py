787236732b5a0b01af97466c2cf4430a5df445cd
Derek M Berger <dmberger.dev@gmail.com>
2020-02-27 14:34:34 -0500

    implement basic testing of automatic unfolding methods

---
+++ empyricalRMT/tests/test_unfold.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pytest
+
+from empyricalRMT.rmt.eigenvalues import Eigenvalues
+from empyricalRMT.rmt.construct import generate_eigs
+
+
+@pytest.mark.fast
+def test_unfold_methods() -> None:
+    eigs = Eigenvalues(generate_eigs(500, seed=2))
+    trimmed = eigs.get_best_trimmed()
+    print("Trim starts and ends:")
+    print(trimmed.vals[0])
+    print(trimmed.vals[-1])
+    assert np.allclose(trimmed.vals[0], -35.84918623729985)
+    assert np.allclose(trimmed.vals[-1], 34.709818777689364)
+
+    unfolded = eigs.trim_unfold_auto()
+    print("Trim starts and ends:")
+    print(unfolded.vals[0])
+    print(unfolded.vals[-1])
+    assert np.allclose(unfolded.vals[0], -2.473290621491799)
+    assert np.allclose(unfolded.vals[-1], 504.2764217889801)
+
