9ad151f1231eb6df41ddb3209410cacbd75ee61d
Derek M Berger <dmberger.dev@gmail.com>
2020-02-29 14:58:22 -0500

    rework ensemble classes

--- empyricalRMT/rmt/ensemble.py
+++ empyricalRMT/rmt/ensemble.py
@@ -1,55 +1,134 @@
 import numpy as np
+
+from abc import ABC, abstractmethod
 from numpy import ndarray
+from scipy.special import gamma
 
-from typing import Tuple
+from typing import Tuple, Union
 
 
-class Poisson:
+class _Ensemble(ABC):
     @staticmethod
-    def spacing_distribution(
-        spacings_range: Tuple[float, float] = (0.0, 3.0)
+    @abstractmethod
+    def nnsd(
+        spacings_range: Tuple[float, float] = (0.0, 3.0),
+        n_points: int = 1000,
+        spacings: ndarray = None,
     ) -> ndarray:
-        s = np.linspace(spacings_range[0], spacings_range[1], 10000)
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def nnnsd(
+        spacings_range: Tuple[float, float] = (0.0, 4.0),
+        n_points: int = 1000,
+        spacings: ndarray = None,
+    ) -> ndarray:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def spectral_rigidity(
+        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50, L: ndarray = None
+    ) -> ndarray:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def level_variance(
+        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50, L: ndarray = None
+    ) -> ndarray:
+        pass
+
+
+class Poisson(_Ensemble):
+    @staticmethod
+    def nnsd(
+        spacings_range: Tuple[float, float] = (0.0, 3.0),
+        n_points: int = 1000,
+        spacings: ndarray = None,
+    ) -> ndarray:
+        s = (
+            spacings
+            if spacings is not None
+            else np.linspace(spacings_range[0], spacings_range[1], n_points)
+        )
         return np.exp(-s)
 
     @staticmethod
-    def spectral_rigidity(
-        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50
+    def nnnsd(
+        spacings_range: Tuple[float, float] = (0.0, 4.0),
+        n_points: int = 1000,
+        spacings: ndarray = None,
     ) -> ndarray:
-        L = np.linspace(min_L, max_L, L_grid_size)
+        def brody_dist(s: ndarray, beta: float) -> ndarray:
+            """See Eq. 8 of
+            Dettmann, C. P., Georgiou, O., & Knight, G. (2017).
+            Spectral statistics of random geometric graphs.
+            EPL (Europhysics Letters), 118(1), 18003.
+            """
+            b1 = beta + 1
+            alpha = gamma((beta + 2) / b1) ** b1
+            return b1 * alpha * s ** beta * np.exp(-alpha * s ** b1)
+
+        s = (
+            spacings
+            if spacings is not None
+            else np.linspace(spacings_range[0], spacings_range[1], n_points)
+        )
+        # return np.sqrt(2) * np.exp(-s)
+        return brody_dist(s, 0.6)
+
+    @staticmethod
+    def spectral_rigidity(
+        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50, L: ndarray = None
+    ) -> ndarray:
+        L = L if L is not None else np.linspace(min_L, max_L, L_grid_size)
         return L / 15 / 2
 
     @staticmethod
     def level_variance(
-        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50
+        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50, L: ndarray = None
     ) -> ndarray:
-        L = np.linspace(min_L, max_L, L_grid_size)
+        L = L if L is not None else np.linspace(min_L, max_L, L_grid_size)
         s = L
         return s / 2
 
 
-class GOE:
+class GOE(_Ensemble):
     @staticmethod
     def nnsd(
-        spacings_range: Tuple[float, float] = (0.0, 3.0), n_points: int = 1000
+        spacings_range: Tuple[float, float] = (0.0, 3.0),
+        n_points: int = 1000,
+        spacings: ndarray = None,
     ) -> ndarray:
         """return expected spacings over the range [spacings.min(), spacings.max()], where
         `spacings` are the spacings calculated from `unfolded`
         """
-        s = np.linspace(spacings_range[0], spacings_range[1], n_points)
+        s = (
+            spacings
+            if spacings is not None
+            else np.linspace(spacings_range[0], spacings_range[1], n_points)
+        )
         p = np.pi
         return ((p * s) / 2) * np.exp(-(p / 4) * s * s)
 
     @staticmethod
     def nnnsd(
-        spacings_range: Tuple[float, float] = (0.0, 3.0), n_points: int = 1000
+        spacings_range: Tuple[float, float] = (0.0, 3.0),
+        n_points: int = 1000,
+        spacings: ndarray = None,
     ) -> ndarray:
         """return expected spacings over the range [spacings.min(), spacings.max()], where
         `spacings` are the spacings calculated from `unfolded`
         """
         # remember, we need to scale by the mean spacing, which in the case of
         # the *next* NNSD, is 2, and not 1. So divide by two below
-        s = np.linspace(spacings_range[0], spacings_range[1], n_points) / 2
+        s = (
+            spacings
+            if spacings is not None
+            else np.linspace(spacings_range[0], spacings_range[1], n_points) / 2
+        )
         p = np.pi
         # fmt: off
         goe = (2**18 / (3**6 * p**3)) * (s**4) * np.exp(-((64 / (9 * p)) * (s * s)))
@@ -58,46 +137,60 @@ class GOE:
 
     @staticmethod
     def spectral_rigidity(
-        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50
+        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50, L: ndarray = None
     ) -> ndarray:
-        L = np.linspace(min_L, max_L, L_grid_size)
+        L = L if L is not None else np.linspace(min_L, max_L, L_grid_size)
         s = L
         p, y = np.pi, np.euler_gamma
         return (1 / (p ** 2)) * (np.log(2 * p * s) + y - 5 / 4 - (p ** 2) / 8)
 
     @staticmethod
     def level_variance(
-        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50
+        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50, L: ndarray = None
     ) -> ndarray:
-        L = np.linspace(min_L, max_L, L_grid_size)
+        L = L if L is not None else np.linspace(min_L, max_L, L_grid_size)
         s = L
         p = np.pi
         return (2 / (p ** 2)) * (np.log(2 * p * s) + np.euler_gamma + 1 - (p ** 2) / 8)
 
 
-class GUE:
+class GUE(_Ensemble):
     @staticmethod
-    def spacing_distribution(
-        spacings_range: Tuple[float, float] = (0.0, 3.0), n_points: int = 1000
+    def nnsd(
+        spacings_range: Tuple[float, float] = (0.0, 3.0),
+        n_points: int = 1000,
+        spacings: ndarray = None,
     ) -> ndarray:
-        s = np.linspace(spacings_range[0], spacings_range[1], 10000)
+        s = (
+            spacings
+            if spacings is not None
+            else np.linspace(spacings_range[0], spacings_range[1], 10000)
+        )
         p = np.pi
         return (32 / p ** 2) * (s * s) * np.exp(-(4 * s * s) / p)
 
     @staticmethod
-    def spectral_rigidity(
-        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50
+    def nnnsd(
+        spacings_range: Tuple[float, float] = (0.0, 4.0),
+        n_points: int = 1000,
+        spacings: ndarray = None,
     ) -> ndarray:
-        L = np.linspace(min_L, max_L, L_grid_size)
+        raise NotImplementedError()
+
+    @staticmethod
+    def spectral_rigidity(
+        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50, L: ndarray = None
+    ) -> ndarray:
+        L = L if L is not None else np.linspace(min_L, max_L, L_grid_size)
         s = L
         p = np.pi
         return (1 / (2 * (p ** 2))) * (np.log(2 * p * s) + np.euler_gamma - 5 / 4)
 
     @staticmethod
     def level_variance(
-        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50
+        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50, L: ndarray = None
     ) -> ndarray:
-        L = np.linspace(min_L, max_L, L_grid_size)
+        L = L if L is not None else np.linspace(min_L, max_L, L_grid_size)
         s = L
         p = np.pi
         return (1 / (p ** 2)) * (np.log(2 * p * s) + np.euler_gamma + 1)
@@ -105,10 +198,16 @@ class GUE:
 
 class GSE:
     @staticmethod
-    def spacing_distribution(
-        spacings_range: Tuple[float, float] = (0.0, 3.0), n_points: int = 1000
+    def nnsd(
+        spacings_range: Tuple[float, float] = (0.0, 3.0),
+        n_points: int = 1000,
+        spacings: ndarray = None,
     ) -> ndarray:
-        s = np.linspace(spacings_range[0], spacings_range[0], 10000)
+        s = (
+            spacings
+            if spacings is not None
+            else np.linspace(spacings_range[0], spacings_range[0], 10000)
+        )
         p = np.pi
         # (2 ** 18 / (3 ** 6 * p ** 3)) * (s ** 4) * np.exp(-((64 / (9 * p)) *
         # (s * s)))
@@ -117,10 +216,18 @@ class GSE:
         # fmt: on
 
     @staticmethod
-    def spectral_rigidity(
-        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50
+    def nnnsd(
+        spacings_range: Tuple[float, float] = (0.0, 4.0),
+        n_points: int = 1000,
+        spacings: ndarray = None,
     ) -> ndarray:
-        L = np.linspace(min_L, max_L, L_grid_size)
+        raise NotImplementedError()
+
+    @staticmethod
+    def spectral_rigidity(
+        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50, L: ndarray = None
+    ) -> ndarray:
+        L = L if L is not None else np.linspace(min_L, max_L, L_grid_size)
         # s = L / np.mean(spacings)
         s = L
         p, y = np.pi, np.euler_gamma
@@ -128,9 +235,13 @@ class GSE:
 
     @staticmethod
     def level_variance(
-        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50
+        min_L: float = 0.5, max_L: float = 20, L_grid_size: int = 50, L: ndarray = None
     ) -> ndarray:
-        L = np.linspace(min_L, max_L, L_grid_size)
+        L = L if L is not None else np.linspace(min_L, max_L, L_grid_size)
         s = L
         p, y = np.pi, np.euler_gamma
         return (1 / (2 * (p ** 2))) * (np.log(4 * p * s) + y + 1 + (p ** 2) / 8)
+
+
+GDE = Poisson
+Ensemble = Union[GOE, GDE, Poisson, GUE, GSE]
