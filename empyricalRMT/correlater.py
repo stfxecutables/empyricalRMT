947189a38fa9efe75c5a142e54a132310b3e73ee
Derek M Berger <dmberger.dev@gmail.com>
2020-03-10 12:59:16 -0400

    add ddof option to correlater

--- empyricalRMT/correlater.py
+++ empyricalRMT/correlater.py
@@ -2,14 +2,15 @@ import numpy as np
 from numpy import ndarray
 
 
-def correlate_fast(arr: ndarray) -> ndarray:
+def correlate_fast(arr: ndarray, ddof: int = 0) -> ndarray:
     """Just a copy of the np.corrcoef source, with extras removed"""
-    c = np.cov(arr)
+    # TODO: use dask or memmaps to allow larger correlation matrices
+    c = np.cov(arr, ddof=ddof)
     d = np.diag(c).reshape(-1, 1)
 
     sd = np.sqrt(d)
     c /= sd
     c /= sd.T
-    # np.clip(c, -1, 1, out=c)  # not needed for real matrices
+    np.clip(c, -1, 1, out=c)
 
     return c
