8c8d7d43e7c05ed3f1b59c3c39a76f37998e8312
Derek M Berger <dmberger.dev@gmail.com>
2020-03-25 15:07:49 -0400

    update version for release

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     author="Derek Berger",
     maintainer="Derek Berger",
     author_email="dmberger.dev@gmail.com",
-    version="0.3.dev1",
+    version="0.4.0",
     description="Eigenvalue unfolding and spectral observable computation",
     url="https://github.com/stfxecutables/empyricalRMT",
     license="MIT",
